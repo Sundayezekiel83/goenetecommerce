8a11f75ade6563d257ddfc940325bda71abe8a6b
sunday ezekiel <sundayezekiel83@gmail.com>
2023-07-12 06:34:36 +0100

    gogleId

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,8 +4,9 @@ import GoogleProvider from "next-auth/providers/google";
 export default NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
-      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
+      clientId:
+        "325672615643-lnfi16crsev1g0ha4rslp1etem6mg2fm.apps.googleusercontent.com",
+      clientSecret: "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",
     }),
   ],
   secret: "f3fdc56be2e9fd84911253a12f0b090f",
